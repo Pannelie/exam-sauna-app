cfc8e94a9660eac317d2ba721e52e2e291a695c9
Annelie Östelid <Annelie.ostelid@outlook.com>
2026-03-10 13:17:53 +0100

    Remove debug logging from admin validation middleware

--- bastuimperiet-backend/src/middlewares/validateAdmin.mjs
+++ bastuimperiet-backend/src/middlewares/validateAdmin.mjs
@@ -4,11 +4,11 @@ import { throwError } from "../utils/throwError.mjs";
 export const validateAdmin = () => ({
     before: (handler) => {
         const { error, value } = adminSchema.validate(handler.event.body);
-        console.log("Admin validation error:", error);
-        console.log("Admin validation value:", value);
 
         if (error) {
             return throwError(error.message, 400);
         }
+
+        handler.event.body = value;
     },
 });
