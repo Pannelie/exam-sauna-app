e51c5f5a30daf65eb0e5c84f8e456d044bc1bd78
Annelie Östelid <Annelie.ostelid@outlook.com>
2026-03-11 12:09:25 +0100

    Update HomePage layout and styles; add favicon and logo images

--- bastuimperiet-frontend/src/pages/HomePage.tsx
+++ bastuimperiet-frontend/src/pages/HomePage.tsx
@@ -1,4 +1,3 @@
-import { styled } from "@mui/material";
 import MenuBar from "../components/MenuBar/MenuBar";
 import Hero from "../components/Hero/Hero";
 import { InfoSection } from "../components/HomeSections/InfoSection";
@@ -6,18 +5,9 @@ import { PricesSection } from "../components/HomeSections/PricesSection";
 import { ContactSection } from "../components/HomeSections/ContactSection";
 import { BookingSection } from "../components/HomeSections/BookingSection";
 
-const StyledDiv = styled("div")(({ theme }) => ({
-    display: "flex",
-    flexDirection: "column",
-    paddingBottom: "calc(64px + env(safe-area-inset-bottom))",
-    [theme.breakpoints.up("md")]: {
-        paddingBottom: 0,
-    },
-}));
-
 export const HomePage = () => {
     return (
-        <StyledDiv>
+        <main className="home_main">
             <Hero />
             <MenuBar />
             <div className="page_container--center">
@@ -26,6 +16,6 @@ export const HomePage = () => {
                 <ContactSection />
                 <BookingSection />
             </div>
-        </StyledDiv>
+        </main>
     );
 };
