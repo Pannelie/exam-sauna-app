e82ece657b5bde9708819a247ac2ced02a127805
Annelie Östelid <Annelie.ostelid@outlook.com>
2026-03-13 09:11:01 +0100

    Refactor BookingSection import path and restore BookingSection component

--- bastuimperiet-frontend/src/pages/HomePage.tsx
+++ bastuimperiet-frontend/src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero/Hero";
 import { InfoSection } from "../components/HomeSections/InfoSection/InfoSection";
 import { PricesSection } from "../components/HomeSections/PricesSection";
 import { ContactSection } from "../components/HomeSections/ContactSection/ContactSection";
-import { BookingSection } from "../features/BookingSection";
+import { BookingSection } from "../features/booking/BookingSection";
 import "./page.css";
 
 export const HomePage = () => {
