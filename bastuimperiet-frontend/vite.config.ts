1303af12111d62d845a7aa9a521e95dc6bc563e3
Annelie Östelid <Annelie.ostelid@outlook.com>
2026-03-24 23:22:10 +0100

    Integrate rollup-plugin-visualizer for build analysis and replace JPG assets with WEBP format to recuce sizing when hosting in S3 bucket

--- bastuimperiet-frontend/vite.config.ts
+++ bastuimperiet-frontend/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        visualizer({
+            open: true, // Detta öppnar kartan automatiskt i webbläsaren efter bygget
+            filename: "stats.html",
+            gzipSize: true,
+        }),
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return "vendor";
+                    }
+                },
+            },
+        },
+    },
+});
