14c21ac6137e5fd6f97636999d7c7537d65a9146
Annelie Östelid <Annelie.ostelid@outlook.com>
2026-03-25 16:01:04 +0100

    Found bug.  called wrong middleware

--- bastuimperiet-backend/src/functions/Admin/getLoggedInAdmin.mjs
+++ bastuimperiet-backend/src/functions/Admin/getLoggedInAdmin.mjs
@@ -1,13 +1,16 @@
 import middy from "@middy/core";
-import { validateAdmin } from "../../middlewares/validateAdmin.mjs";
+import { verifyAdminToken } from "../../middlewares/verifyAdminToken.js";
 import { getAdminByEmail } from "../../services/adminService.mjs";
 import { errorHandler } from "../../middlewares/errorHandler.js";
 
 export const handler = middy(async (event) => {
+    console.log("event.admin:", event.admin);
+    console.log("event:", event);
     const email = event.admin.email;
 
     const admin = await getAdminByEmail(process.env.TABLE_NAME, email);
 
+    console.log("profile Admin fetched:", admin);
     if (!admin) {
         const error = new Error("Admin hittades inte");
         error.statusCode = 404; // <-- viktigt
@@ -18,5 +21,5 @@ export const handler = middy(async (event) => {
         body: JSON.stringify(admin),
     };
 })
-    .use(validateAdmin())
+    .use(verifyAdminToken())
     .use(errorHandler());
