21cbf123648744baad25d2dcaea6147e3c7e9798
Annelie Östelid <Annelie.ostelid@outlook.com>
2026-03-30 23:22:58 +0200

    one input a row

--- bastuimperiet-frontend/src/features/booking/components/Steps/StepContact.tsx
+++ bastuimperiet-frontend/src/features/booking/components/Steps/StepContact.tsx
@@ -17,54 +17,49 @@ export const StepContact = ({ data, updateField, back, next, errors, isMobile }:
     return (
         <section className="step_container">
             <Stack spacing={2}>
-                <Stack direction="row" spacing={2}>
-                    <StyledTextField
-                        label="Namn"
-                        value={data.name}
-                        onChange={(e) => updateField("name", e.target.value)}
-                        error={!!errors.name}
-                        helperText={errors.name}
-                    />
-                    <StyledTextField
-                        label="Telefonnummer"
-                        value={data.phone}
-                        onChange={(e) => updateField("phone", e.target.value)}
-                        error={!!errors.phone}
-                        helperText={errors.phone}
-                    />
-                </Stack>
-                <Stack spacing={2}>
-                    <StyledTextField
-                        label="Epostadress"
-                        value={data.email}
-                        onChange={(e) => updateField("email", e.target.value)}
-                        error={!!errors.email}
-                        helperText={errors.email}
-                    />
-                    <StyledTextField
-                        label="Adress"
-                        value={data.address}
-                        onChange={(e) => updateField("address", e.target.value)}
-                        error={!!errors.address}
-                        helperText={errors.address}
-                    />
-                </Stack>
-                <Stack direction="row" spacing={2}>
-                    <StyledTextField
-                        label="Postnummer"
-                        value={data.postalCode}
-                        onChange={(e) => updateField("postalCode", e.target.value)}
-                        error={!!errors.postalCode}
-                        helperText={errors.postalCode}
-                    />
-                    <StyledTextField
-                        label="Stad"
-                        value={data.city}
-                        onChange={(e) => updateField("city", e.target.value)}
-                        error={!!errors.city}
-                        helperText={errors.city}
-                    />
-                </Stack>
+                <StyledTextField
+                    label="Namn"
+                    value={data.name}
+                    onChange={(e) => updateField("name", e.target.value)}
+                    error={!!errors.name}
+                    helperText={errors.name}
+                />
+                <StyledTextField
+                    label="Telefonnummer"
+                    value={data.phone}
+                    onChange={(e) => updateField("phone", e.target.value)}
+                    error={!!errors.phone}
+                    helperText={errors.phone}
+                />
+
+                <StyledTextField
+                    label="Epostadress"
+                    value={data.email}
+                    onChange={(e) => updateField("email", e.target.value)}
+                    error={!!errors.email}
+                    helperText={errors.email}
+                />
+                <StyledTextField
+                    label="Adress"
+                    value={data.address}
+                    onChange={(e) => updateField("address", e.target.value)}
+                    error={!!errors.address}
+                    helperText={errors.address}
+                />
+                <StyledTextField
+                    label="Postnummer"
+                    value={data.postalCode}
+                    onChange={(e) => updateField("postalCode", e.target.value)}
+                    error={!!errors.postalCode}
+                    helperText={errors.postalCode}
+                />
+                <StyledTextField
+                    label="Stad"
+                    value={data.city}
+                    onChange={(e) => updateField("city", e.target.value)}
+                    error={!!errors.city}
+                    helperText={errors.city}
+                />
             </Stack>
             <Stack direction="row" spacing={2} justifyContent={"space-between"}>
                 <FormButton type="back" variant="outlined" onClick={back} text="Tillbaka" isMobile={isMobile} />
